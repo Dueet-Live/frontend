c14e84c2b683f9ced625a66e424e6b6bf2465d8d
Liu Guangyuan (Gary) <garyliu2122@gmail.com>
2020-10-29 01:29:45 +0800

    Quiten playback sound temporarily

--- src/components/Game/GameView.tsx
+++ src/components/Game/GameView.tsx
@@ -165,7 +165,8 @@ const GameView: React.FC<Props> = ({
           note.midi,
           note.time + delayedStartTime,
           note.duration,
-          note.velocity
+          // TODO: this should be done in the JSON file
+          note.velocity * 0.5
         );
         handlers.push(handler);
         // Schedule 1 sec before the note play
