b7472abd7bf35136c91e53874d0bcd40cc8fead4
Liu Guangyuan (Gary) <garyliu2122@gmail.com>
2020-10-31 01:08:54 +0800

    Remove left padding of tool bar when width < 400

--- src/components/shared/RoomHeader.tsx
+++ src/components/shared/RoomHeader.tsx
@@ -1,14 +1,19 @@
 import { AppBar, makeStyles, Toolbar, ToolbarProps } from '@material-ui/core';
 import React from 'react';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
   },
   appBar: {
     backgroundColor: '#FFF',
   },
-});
+  toolBar: {
+    [theme.breakpoints.down('xs')]: {
+      paddingLeft: 0,
+    },
+  },
+}));
 
 const RoomHeader: React.FC<ToolbarProps> = ({ children, ...props }) => {
   const classes = useStyles();
@@ -16,7 +21,9 @@ const RoomHeader: React.FC<ToolbarProps> = ({ children, ...props }) => {
   return (
     <div className={classes.root}>
       <AppBar position="absolute" className={classes.appBar}>
-        <Toolbar {...props}>{children}</Toolbar>
+        <Toolbar {...props} className={classes.toolBar}>
+          {children}
+        </Toolbar>
       </AppBar>
       <Toolbar /> {/* To take up space */}
     </div>
--- src/theme.ts
+++ src/theme.ts
@@ -12,6 +12,15 @@ declare module '@material-ui/core/styles/createPalette' {
 }
 
 const theme = createMuiTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 400,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
   palette: {
     primary: {
       main: '#904AE9',
